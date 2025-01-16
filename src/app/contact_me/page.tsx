83431e97637aa035ef0bdb9c78e68f1e4c4f08d3
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-01-16 17:19:32 +0300

    add sample code

--- src/app/contact_me/page.tsx
+++ src/app/contact_me/page.tsx
@@ -1,6 +1,36 @@
 'use client'
+import {useState} from "react";
+
 export default function ContactMePage(){
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const date = new Date();
   return (<div>
-    Contact me PAge
+    <form className={'flex flex-col gap-8'}>
+      <div className={'flex flex-col gap-1'}>
+        <label htmlFor="name" className={'text-secondary-grey'}>_name:</label>
+        <input type="text" id={'name'} name={'name'} className={'w-[327px] border border-lines bg-primary-blue'}value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div className={'flex flex-col gap-1'}>
+        <label htmlFor={'email'} className={'text-secondary-grey'}>_email:</label>
+        <input type="text" id={'email'} name={'email'} className={'w-[327px] border border-lines bg-primary-blue'} value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div className={'flex flex-col gap-1'}>
+        <label htmlFor={'message'} className={'text-secondary-grey'}>_message:</label>
+        <textarea name={'message'} className={'w-[327px] h-[145px] border border-lines bg-primary-blue'} value={message}  onChange={(e) => setMessage(e.target.value)} />
+      </div>
+      <button type={'submit'}>submit-message</button>
+    </form>
+    <div>
+      <div>
+        <span>{name}</span>
+      </div>
+      <div><span>{email}</span></div>
+      <div><span>{message}</span></div>
+      <div>{date.toDateString()}</div>
+
+    </div>
   </div>)
 }
