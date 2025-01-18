e3f0dc3e88528e7d5e81d5684da775ce60a42d0d
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-01-18 11:43:57 +0300

    fix mobile menu

--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -1,9 +1,18 @@
 'use client'
 
-export function Burger({isOpen, setIsOpen}:{isOpen:boolean, setIsOpen:()=>void}) {
+import {Dispatch, SetStateAction} from "react";
 
+type BurgerProps = {
+  isOpen: boolean
+  setIsOpen: Dispatch<SetStateAction<boolean>>
+}
+
+export function Burger({isOpen, setIsOpen}:BurgerProps) {
+const handleClick = () =>{
+  setIsOpen((isOpen)=>!isOpen)
+  }
   return (
-      <button onClick={()=>setIsOpen()} className={'flex flex-col justify-center items-center'}>
+      <button onClick={handleClick} className={'flex flex-col justify-center items-center'}>
         <span className={`bg-secondary-grey block transition-all duration-300 ease-out 
                     h-[2px] w-6 rounded-sm ${isOpen ?
             'rotate-45 translate-y-[7px]' : ''
--- src/widgets/MobileMenu/MobileMenu.tsx
+++ src/widgets/MobileMenu/MobileMenu.tsx
@@ -1,29 +1,35 @@
 'use client'
 import {Burger} from "@/components/Burger";
-import {Dispatch, SetStateAction} from "react";
-import Link from "next/link";
+import {Dispatch, SetStateAction, useEffect} from "react";
+import {usePathname, useRouter} from "next/navigation";
 
 export function MobileMenu({isOpen, setIsOpen}:{isOpen:boolean, setIsOpen: Dispatch<SetStateAction<boolean>>}) {
+  const router = useRouter();
+  const pathName = usePathname()
 
-  const handleClick = () => {
-    setIsOpen((isOpen)=>!isOpen)
+  const handleClick = async (path:string) => {
+    await router.push(`/${path}`)
   }
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathName, setIsOpen]);
 
   return (
       <div className={'flex flex-col w-full max-w-screen-md border-b border-b-lines md:hidden'}>
         <div className={'flex justify-between items-center h-[50px] p-[18px]'}>
           <span>Pavel_Retunskih</span>
-          <Burger isOpen={isOpen} setIsOpen={handleClick}/>
+          <Burger isOpen={isOpen} setIsOpen={setIsOpen}/>
         </div>
 
     {isOpen && <div className={' w-[94%] h-full mx-auto bg-primary-blue mt-[55px] flex' +
         ' flex-col gap-[35px]' +
         ' items-center'}>
       <ul>
-        <li onClick={()=>setIsOpen(false)}><Link href="/hello">_hello</Link></li>
-        <li onClick={()=>setIsOpen(false)} ><Link href="/about_me">_about-me</Link></li>
-        <li onClick={()=>setIsOpen(false)}><Link href="/projects">_projects</Link></li>
-        <li onClick={()=>setIsOpen(false)}><Link href="/contact_me">_contact-me</Link></li>
+        <li onClick={()=>handleClick('hello') }>_hello</li>
+        <li onClick={()=>handleClick('about_me')} >_about-me</li>
+        <li onClick={()=>handleClick('projects')}>_projects</li>
+        <li onClick={()=>handleClick('contact_me')}>_contact-me</li>
       </ul>
     </div>}
   </div>)
