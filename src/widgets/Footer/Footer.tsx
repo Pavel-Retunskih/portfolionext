58f4c027d3f015748787385d6c0ff918bb868803
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-01-19 18:45:54 +0300

    add icons

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -1,14 +1,18 @@
-export function Footer(){
+import GithubIcon from "@/assets/svg/GithubIcon";
+import TelegramIcon from "@/assets/svg/TelegramIcon";
+import LinkedInIcon from "@/assets/svg/LinkedInIcon";
+
+export function Footer() {
   return (
       <div className={'h-[50px] w-full flex items-center justify-between'}>
-        <div>
+        <div className={'flex items-center gap-3'}>
           <p>find me in:</p>
-          <div></div>
-          <div></div>
+          <TelegramIcon className={'hover:text-secondary-white'}/>
+          <LinkedInIcon className={'hover:text-secondary-white'}/>
         </div>
-        <div>
+        <div className={'flex items-center gap-2.5'}>
           <span>@username</span>
-          <div></div>
+          <GithubIcon className={'hover:text-secondary-white'}/>
         </div>
       </div>
 
