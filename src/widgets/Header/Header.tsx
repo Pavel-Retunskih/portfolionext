93e84195c578eef24ace95654ab0a300e5948df5
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-02-26 22:16:59 +0300

    fix rerenders

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,19 +1,28 @@
 'use client'
 
 import Link from "next/link";
-import {usePathname} from "next/navigation";
+
 import {memo, useMemo} from "react";
 
+import {useCurrentPath} from "@/utils/PathnameProvider/PathnameProvider";
+
 export const Header = memo(() => {
-  const currentPath = usePathname()
+  const currentPath = useCurrentPath()
+  const activeLinkClass = 'border-b-2 border-b-accent-orange'
+  const linkClasses = useMemo(
+      () => ({
+        "/hello": currentPath === "/hello" ? activeLinkClass : "",
+        "/about_me": currentPath === "/about_me" ? activeLinkClass : "",
+        "/projects": currentPath === "/projects" ? activeLinkClass : "",
+        "/contact_me": currentPath === "/contact_me" ? activeLinkClass : "",
+      }),
+      [currentPath]
+  );
   const linkClass = 'text-[16px] border-r-2 border-r-lines flex items-center h-full ' +
       ' px-8'
-  const activeLinkClass = 'border-b-2 border-b-accent-orange'
+
   console.log('Header render')
-  const getToggleItemClass = useMemo(() => {
-    return (path: string) =>
-        `${currentPath === path ? activeLinkClass : ' '}`;
-  }, [currentPath]);
+
   return (
       <header className={'hidden md:block' + ' ' + ' w-full'}>
         <nav className={'flex h-[45px] w-full'}>
@@ -24,15 +33,15 @@ export const Header = memo(() => {
           <div className={'flex items-center flex-grow h-full justify-between'}>
             <div className={'flex h-full items-center'}>
               <Link href="/hello"
-                    className={getToggleItemClass('/hello') + " " + linkClass}>_hello</Link>
+                    className={`${linkClasses["/hello"]} ${linkClass}`}>_hello</Link>
               <Link href="/about_me"
-                    className={getToggleItemClass('/about_me') + " " + linkClass + ' ' + 'text-nowrap'}>_about-me</Link>
+                    className={`${linkClasses["/about_me"]} ${linkClass}`}>_about-me</Link>
               <Link href="/projects"
-                    className={getToggleItemClass('/projects') + " " + linkClass + ' ' + 'text-nowrap'}>_projects</Link>
+                    className={`${linkClasses["/projects"]} ${linkClass}`}>_projects</Link>
             </div>
             <div className={'flex items-center h-full'}>
               <Link href="/contact_me"
-                    className={getToggleItemClass('/contact_me') + '   text-[16px] border-l border-l-lines flex items-center h-full  px-8'}>contact-me</Link>
+                    className={`${linkClasses["/contact_me"]} text-[16px] border-l border-l-lines flex items-center h-full px-8`}>contact-me</Link>
             </div>
           </div>
 
