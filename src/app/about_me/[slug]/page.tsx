4ac8dcdd51c8985bdb26f0ab3f04cddb0ba5c28d
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-02-26 20:21:05 +0300

    fix data fetching

--- src/app/about_me/[slug]/page.tsx
+++ src/app/about_me/[slug]/page.tsx
@@ -8,22 +8,21 @@ const URL = process.env["NEXT_PUBLIC_API_URL"]
 //   return await res.json()
 // }
 
-// export async function generateStaticParams() {
-//   if (!URL) {
-//     throw new Error('NEXT_PUBLIC_API_URL is not defined')
-//   }
-//   const res = await fetch('http://localhost:3000/api', {method: 'GET'})
-//   const data = await res.json()
-//   console.log(data)
-//   return data.pages.map((page: { title: string }) => ({slug: page.title}))
-// }
+export async function generateStaticParams() {
+  if (!URL) {
+    throw new Error('NEXT_PUBLIC_API_URL is not defined')
+  }
+  const res = await fetch(`https://re-tune.xyz/api`, {method: 'GET'})
+  const data = await res.json()
+  return data.pages.map((page: { title: string }) => ({slug: page.title}))
+}
 
 export default async function AboutPage({params}: { params: Promise<{ slug: string }> }) {
   const {slug} = await params
   if (!URL) {
     throw new Error('NEXT_PUBLIC_API_URL is not defined')
   }
-  const res = await fetch(`${URL}/api/${slug}`, {method: 'GET'})
+  const res = await fetch(`https://re-tune.xyz/api/${slug}`, {method: 'GET'})
   if (!res.ok) {
     throw new Error(`HTTP error! status: ${res.status}`);
   }
