30560925fdce8dc546d116374ad7d75f56aecce1
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-03-01 22:43:22 +0300

    add some logic for projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,57 @@
 'use client'
-export default function ProjectsPage(){
-  return (<div>
-    Projects PAge
-  </div>)
+import {useState} from "react";
+
+const projects = [
+  {title: "react simple app", description: "work", technologies: ["react", "redux",]},
+  {title: "next app", description: "work", technologies: ["next",]},
+  {title: "redux toolkit example", description: "work", technologies: ["redux",]},
+  {title: "asd", description: "work", technologies: ["react", "redux", "typescript"]},
+  {title: "asdasd", description: "work", technologies: ["next", "typescript"]},
+  {title: "asdasdasd", description: "work", technologies: ["redux", "typescript"]},
+];
+
+const categories = ["react", "next", "redux", "typescript"];
+
+export default function Projects() {
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
+  const handleSelectCategory = (category: string) => {
+    if (selectedCategories.includes(category)) {
+      setSelectedCategories((prev) => prev.filter((c) => c !== category));
+    } else {
+      setSelectedCategories((prev) => [...prev, category]);
+    }
+  }
+
+  const filteredProjects = selectedCategories.length === 0 ? projects : projects.filter((project) => project.technologies.some((category) => selectedCategories.includes(category)));
+
+
+  console.log(selectedCategories)
+  return <div>
+    <div>
+      <p>Фильтр</p>
+      <ul>
+        {categories.map((category) => <li key={category}>
+              <label>
+                <input type={'checkbox'} onChange={() => handleSelectCategory(category)}
+                       checked={selectedCategories.includes(category)}/>
+                {category}
+              </label>
+            </li>
+        )}
+      </ul>
+    </div>
+    <div>
+      <p>Проекты</p>
+      <ul>
+        {filteredProjects.map((project) => <li key={project.title}>
+          <span>{project.title}</span>
+          <span>{project.description}</span>
+        </li>)}
+      </ul>
+    </div>
+
+
+  </div>
 }
+
